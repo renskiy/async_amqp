bae0c68b90a521e937e541e7f0c2b845eecc1df0
Rinat Khabibiev <srenskiy@gmail.com>
2014-04-12 19:00:41 +0400

    message producer

---
+++ source/producer.py
@@ -0,0 +1,18 @@
+from kombu import Connection, Exchange
+
+dsn = 'amqp://async_rabbitmq:async_rabbitmq@localhost:5672/async_rabbitmq'
+
+
+def send_messages(number_of_messages=1):
+    connection = Connection(dsn)
+    exchange = Exchange(name='tasks')
+    producer = connection.Producer(exchange=exchange, auto_declare=False)
+    for message_number in range(number_of_messages):
+        routing_key = 'task.mail.{}'.format(message_number)
+        message = 'This is message #{}'.format(message_number)
+        producer.publish(message, routing_key=routing_key)
+    print('Successfully sent {} messages'.format(number_of_messages))
+
+
+if __name__ == '__main__':
+    send_messages(10)
