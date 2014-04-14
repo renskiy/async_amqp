8d8f38838ad284313d3cf0d1e694df6f84515ea3
Rinat Khabibiev <rinat@banki.ru>
2014-04-14 12:34:23 +0400

    added sync consumer

---
+++ source/consumer/sync.py
@@ -0,0 +1,29 @@
+import kombu
+import time
+
+dsn = 'amqp://async_rabbitmq:async_rabbitmq@localhost:5672/async_rabbitmq'
+
+
+def do_work(body, message):
+    time.sleep(1)  # some useful work
+    print(body)
+    message.ack()
+
+
+def on_message(body, message):
+    do_work(body, message)
+
+
+def main():
+    try:
+        with kombu.Connection(dsn) as connection:
+            queues = (kombu.Queue(name='mail'), )
+            with connection.Consumer(queues=queues, callbacks=(on_message, )):
+                while True:
+                    connection.drain_events()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
