7019bdb1f9e41f9d9da34dbbe7653e8983a2fcdf
Rinat Khabibiev <rinat@banki.ru>
2014-04-15 15:18:03 +0400

    added async Consumer using semaphore

---
+++ source/consumer/semaphore.py
@@ -0,0 +1,47 @@
+import gevent.monkey
+import gevent.lock
+import kombu
+import logging
+import time
+
+gevent.monkey.patch_all()
+
+dsn = 'amqp://async_rabbitmq:async_rabbitmq@localhost:5672/async_rabbitmq'
+
+greenlets = {}
+
+logging.basicConfig(level='DEBUG')
+
+semaphore = gevent.lock.Semaphore(5)
+
+
+def do_work(body, message):
+    time.sleep(1)  # some useful work
+    print(body)
+    message.ack()
+    semaphore.release()
+    del greenlets[id(gevent.getcurrent())]
+
+
+def on_message(body, message):
+    greenlet = gevent.spawn(do_work, body, message)
+    greenlets[id(greenlet)] = greenlet
+
+
+def main():
+    try:
+        with kombu.Connection(dsn) as connection:
+            queue = kombu.Queue(name='mail')
+            with connection.Consumer(
+                queues=(queue, ),
+                callbacks=(on_message, ),
+                auto_declare=False,
+            ):
+                while True:
+                    semaphore.acquire() and connection.drain_events()
+    except KeyboardInterrupt:
+        gevent.joinall(greenlets.values())
+
+
+if __name__ == '__main__':
+    main()
